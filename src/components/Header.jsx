3fa2c0404718dc916d8974ff27d2e3be0be06ddf
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-02 09:10:05 +0530

    added the advanced nav and headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,51 @@ import {
 	faTaxi,
 } from '@fortawesome/free-solid-svg-icons';
 import { DateRange } from 'react-date-range';
+import { useState } from 'react';
 
-const Header = () => {
-	// use styled components for the animations and the errors found
+// needed css files for the calender UI
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+import { format } from 'date-fns';
+
+const Header = ({ type }) => {
+	const [openDate, setOpenDate] = useState(false);
+
+	const [date, setDate] = useState([
+		{
+			startDate: new Date(),
+			endDate: new Date(),
+			key: 'selection',
+		},
+	]);
+
+	const [openOptions, setOpenOptions] = useState(false);
+
+	const [options, setOptions] = useState({
+		adult: 1,
+		children: 0,
+		rooms: 1,
+	});
+
+	const handleOption = (name, operation) => {
+		setOptions((prev) => {
+			return {
+				...prev,
+				[name]: operation === 'i' ? options[name] + 1 : options[name] - 1,
+			};
+		});
+	};
+
 	return (
 		<div className="bg-blue-900 text-white flex justify-center relative">
-			<div className="w-full max-w-5xl mt-5 mb-[100px] ml-0 mr-0">
+			<div
+				className={
+					type === 'list'
+						? 'w-full max-w-5xl mt-5 mb-0 ml-0 mr-0'
+						: 'w-full max-w-5xl mt-5 mb-[100px] ml-0 mr-0'
+				}
+			>
 				<div className="flex gap-10 mb-12">
 					<div className="flex items-center gap-2.5 hover:bg-white hover:text-black px-2 py-2 rounded-2xl">
 						<FontAwesomeIcon icon={faBed} />
@@ -36,42 +75,131 @@ const Header = () => {
 						<span>Airport taxis</span>
 					</div>
 				</div>
-				<h1 className="font-semibold text-3xl">
-					A Life of Dicount's? It's a Genius Idea!
-				</h1>
-				<p className="my-5 mx-0">
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt,
-					delectus!
-				</p>
-				<button className="bg-blue-500 text-white font-semibold border-none p-2.5 cursor-pointer">
-					Sign in/ Register
-				</button>
-
-				<div className="h-7 bg-white border-yellow-400 border-2 flex items-center justify-around px-0 py-5 rounded-2 absolute bottom-6 w-full max-w-5xl">
-					<div className="flex items-center gap-[10px] ">
-						<FontAwesomeIcon icon={faBed} className="text-gray-400" />
-						<input
-							type="text"
-							placeholder="where are you going?"
-							className="border-none outline-none text-black"
-						/>
-					</div>
-					<div className="flex items-center gap-[10px] ">
-						<FontAwesomeIcon icon={faCalendarDays} className="text-gray-400" />
-						<span className="text-gray-400 cursor-pointer">Date to Date</span>
-					</div>
-					<div className="flex items-center gap-[10px] ">
-						<FontAwesomeIcon icon={faPerson} className="text-gray-400" />
-						<span className="text-gray-400 cursor-pointer">
-							2 Adults 2 Children 1 Room
-						</span>
-					</div>
-					<div className="flex items-center gap-[10px] ">
-						<button className="bg-blue-500 text-white font-semibold border-none py-[5px] px-2.5 cursor-pointer">
-							Search
+				{type !== 'list' && (
+					<>
+						<h1 className="font-semibold text-3xl">
+							A Life of Dicount's? It's a Genius Idea!
+						</h1>
+						<p className="my-5 mx-0">
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt,
+							delectus!
+						</p>
+						<button className="bg-blue-500 text-white font-semibold border-none p-2.5 cursor-pointer">
+							Sign in/ Register
 						</button>
-					</div>
-				</div>
+						<div className="h-12 bg-white border-yellow-400 border-2 flex items-center justify-around px-0 py-5 rounded-2 absolute bottom-6 w-full max-w-5xl rounded-lg">
+							<div className="flex items-center gap-[10px] ">
+								<FontAwesomeIcon icon={faBed} className="text-gray-400" />
+								<input
+									type="text"
+									placeholder="where are you going?"
+									className="border-none outline-none text-black"
+								/>
+							</div>
+							<div className="flex items-center gap-[10px] ">
+								<FontAwesomeIcon
+									icon={faCalendarDays}
+									className="text-gray-400"
+								/>
+								<span
+									className="text-gray-400 cursor-pointer"
+									onClick={() => setOpenDate(!openDate)} // if its true set to false else if false set to true
+								>
+									{`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(
+										date[0].endDate,
+										'MM/dd/yyyy'
+									)}`}
+								</span>
+
+								{/* calender comp */}
+								{openDate && (
+									<DateRange
+										editableDateInputs={true}
+										onChange={(item) => setDate([item.selection])}
+										moveRangeOnFirstSelection={false}
+										ranges={date}
+										className="absolute top-12"
+									/>
+								)}
+							</div>
+							<div className="flex items-center gap-[10px] ">
+								<FontAwesomeIcon icon={faPerson} className="text-gray-400" />
+								<span
+									onClick={() => setOpenOptions(!openOptions)}
+									className="text-gray-400 cursor-pointer"
+								>
+									{`${options.adult} adult . ${options.children} children . ${options.rooms} room`}
+								</span>
+								{openOptions && (
+									<div className="absolute top-[50px] bg-white text-gray-600 rounded-[5px] shadow-2xl">
+										<div className="w-48 flex justify-between m-2.5">
+											<span className="optiontext">Adult</span>
+											<div className="flex items-center gap-2 text-[16px] text-black">
+												<button
+													disabled={options.adult <= 1}
+													onClick={() => handleOption('adult', 'd')}
+													className="w-8 h-8 border border-blue-600 text-blue-600 cursor-pointer bg-white disabled:cursor-not-allowed"
+												>
+													-
+												</button>
+												<span className="">{options.adult}</span>
+												<button
+													onClick={() => handleOption('adult', 'i')}
+													className="w-8 h-8 border border-blue-600 text-blue-600 cursor-pointer bg-white"
+												>
+													+
+												</button>
+											</div>
+										</div>
+										<div className="w-48 flex justify-between m-2.5">
+											<span className="optiontext">Children</span>
+											<div className="flex items-center gap-2 text-[16px] text-black">
+												<button
+													disabled={options.children <= 0}
+													onClick={() => handleOption('children', 'd')}
+													className="w-8 h-8 border border-blue-600 text-blue-600 cursor-pointer bg-white disabled:cursor-not-allowed"
+												>
+													-
+												</button>
+												<span className="">{options.children}</span>
+												<button
+													onClick={() => handleOption('children', 'i')}
+													className="w-8 h-8 border border-blue-600 text-blue-600 cursor-pointer bg-white"
+												>
+													+
+												</button>
+											</div>
+										</div>
+										<div className="w-48 flex justify-between m-2.5">
+											<span className="optiontext">Rooms</span>
+											<div className="flex items-center gap-2 text-[16px] text-black">
+												<button
+													disabled={options.rooms <= 1}
+													onClick={() => handleOption('rooms', 'd')}
+													className="w-8 h-8 border border-blue-600 text-blue-600 cursor-pointer bg-white disabled:cursor-not-allowed"
+												>
+													-
+												</button>
+												<span className="">{options.rooms}</span>
+												<button
+													onClick={() => handleOption('rooms', 'i')}
+													className="w-8 h-8 border border-blue-600 text-blue-600 cursor-pointer bg-white"
+												>
+													+
+												</button>
+											</div>
+										</div>
+									</div>
+								)}
+							</div>
+							<div className="flex items-center gap-[10px] ">
+								<button className="bg-blue-500 text-white font-semibold border-none py-[5px] px-2.5 cursor-pointer rounded">
+									Search
+								</button>
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Navbar from '../../components/Navbar';
 
 const List = () => {
-	return <div>List</div>;
+	return (
+		<div>
+			<Navbar />
+			<Header type="list" />
+		</div>
+	);
 };
 
 export default List;
