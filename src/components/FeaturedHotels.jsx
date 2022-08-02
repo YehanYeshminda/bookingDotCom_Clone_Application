1e6745a27610c87b074ae8927eb4623f4a962465
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-02 10:17:47 +0530

    adde gome section list sections all done

---
+++ src/components/FeaturedHotels.jsx
@@ -0,0 +1,72 @@
+const FeaturedHotels = () => {
+	return (
+		<div className="w-full max-w-5xl flex justify-between gap-5">
+			<div className="flex-1 gap-2.5 flex flex-col">
+				<img
+					src="https://cf.bstatic.com/xdata/images/hotel/max500/34497879.jpg?k=d27391d1eaeb691fafed1ae7fd6cedcfa7d2634113ed446614bd2389ecb1bfc6&o="
+					alt=""
+					className="w-full h-full"
+				/>
+				<span className="font-extrabold">Transz Hotel</span>
+				<span className="font-normal">Hikkaduwa</span>
+				<span className="font-semibold">Starting from $120</span>
+				<div className="rating">
+					<button className="bg-blue-800 text-white border-none px-1 py-1 mr-2.5 font-bold">
+						8.9
+					</button>
+					<span className="text-lg">Excellent</span>
+				</div>
+			</div>
+			<div className="flex-1 gap-2.5 flex flex-col">
+				<img
+					src="https://cf.bstatic.com/xdata/images/hotel/max500/38437078.jpg?k=241519692574af90306f0dbec0121eacfddc9a183df3ea34a7b7c8f2b4f5febb&o="
+					alt=""
+					className="w-full h-full"
+				/>
+				<span className="font-extrabold">Transz Hotel</span>
+				<span className="font-normal">Hikkaduwa</span>
+				<span className="font-semibold">Starting from $120</span>
+				<div className="rating">
+					<button className="bg-blue-800 text-white border-none px-1 py-1 mr-2.5 font-bold">
+						8.9
+					</button>
+					<span className="text-lg">Excellent</span>
+				</div>
+			</div>
+			<div className="flex-1 gap-2.5 flex flex-col">
+				<img
+					src="https://cf.bstatic.com/xdata/images/hotel/max500/74594395.jpg?k=d5a71c0578774c302d2b8854482beaa8785077c616f956aac74766124023a319&o="
+					alt=""
+					className="w-full h-full"
+				/>
+				<span className="font-extrabold">Transz Hotel</span>
+				<span className="font-normal">Hikkaduwa</span>
+				<span className="font-semibold">Starting from $120</span>
+				<div className="rating">
+					<button className="bg-blue-800 text-white border-none px-1 py-1 mr-2.5 font-bold">
+						8.9
+					</button>
+					<span className="text-lg">Excellent</span>
+				</div>
+			</div>
+			<div className="flex-1 gap-2.5 flex flex-col">
+				<img
+					src="https://cf.bstatic.com/xdata/images/hotel/max500/95656033.jpg?k=9b09d1fa19486e9e792c3406db580f92b736ed12ea924fb365d1ed21b60fc2da&o="
+					alt=""
+					className="w-full h-full"
+				/>
+				<span className="font-extrabold">Transz Hotel</span>
+				<span className="font-normal">Hikkaduwa</span>
+				<span className="font-semibold">Starting from $120</span>
+				<div className="rating">
+					<button className="bg-blue-800 text-white border-none px-1 py-1 mr-2.5 font-bold">
+						8.9
+					</button>
+					<span className="text-lg">Excellent</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default FeaturedHotels;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import FeaturedCities from '../../components/FeaturedCities';
+import FeaturedHotels from '../../components/FeaturedHotels';
 import Header from '../../components/Header';
 import Navbar from '../../components/Navbar';
+import PropertyList from '../../components/PropertyList';
 
 const Home = () => {
 	return (
@@ -10,7 +12,11 @@ const Home = () => {
 			<div className="mt-12 flex flex-col items-center gap-8">
 				<FeaturedCities />
 
-				<h1 className="w-5xl">Browse By Property Type!</h1>
+				<h1 className="w-5xl text-2xl font-medium">Browse By Property Type!</h1>
+				<PropertyList />
+
+				<h1 className="w-5xl text-2xl font-medium">Home Guests Love!</h1>
+				<FeaturedHotels />
 			</div>
 		</div>
 	);
