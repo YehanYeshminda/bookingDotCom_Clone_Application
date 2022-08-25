3dd3aa2946738a1bcaf09b1f8c6b5f9107c2a099
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-25 21:50:46 +0530

    finished most of the backend functionalities

--- backend/controllers/room-controller.js
+++ backend/controllers/room-controller.js
@@ -1,2 +1,89 @@
-import Room from '../models/Room';
+import Hotel from '../models/Hotel.js';
+import Room from '../models/Room.js';
+
 import { createError } from '../utils/error.js';
+
+// when creating a new room the id should be added to the hotel array on the room
+
+// creating a room and then adding the room id into the hotels array
+export const createRoom = async (req, res, next) => {
+	const hotelId = req.params.hotelId; // we enter the hotel id using the params
+	const newRoom = new Room(req.body);
+
+	try {
+		const savedRoom = await newRoom.save();
+
+		// this is used to update the hotel
+		try {
+			await Hotel.findByIdAndUpdate(hotelId, {
+				// push lets us push any type of value into the array inside of the mongo db
+				$push: { rooms: savedRoom._id },
+			});
+		} catch (error) {
+			next(error);
+		}
+
+		res.status(200).json(savedRoom);
+	} catch (error) {
+		next(error);
+	}
+};
+
+// update a existing room
+export const updateRoom = async (req, res, next) => {
+	try {
+		const updatedRoom = await Room.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body, // after updating this will return the new version of the updated one because new = true
+			},
+			{ new: true }
+		);
+		res.status(200).json(updatedRoom);
+	} catch (error) {
+		next(error);
+	}
+};
+
+// delete a existing room
+export const deleteRooms = async (req, res, next) => {
+	const hotelId = req.params.hotelId; // we enter the hotel id using the params
+
+	try {
+		await Room.findByIdAndDelete(req.params.id);
+
+		try {
+			await Hotel.findByIdAndUpdate(hotelId, {
+				// pull lets us remove any type of value from an array inside of the mongo db
+				$pull: { rooms: req.params.id },
+			});
+		} catch (error) {
+			next(error);
+		}
+
+		res.status(200).json('Room has been deleted');
+	} catch (error) {
+		next(error);
+	}
+};
+
+// get a unique room
+export const getUniqueRoom = async (req, res, next) => {
+	try {
+		const specificRoom = await Room.findById(req.params.id);
+		res.status(200).json(specificRoom);
+	} catch (error) {
+		next(error);
+	}
+};
+
+// get all room
+export const getAllRooms = async (req, res, next) => {
+	try {
+		const allRooms = await Room.find();
+		res.status(200).json(allRooms);
+	} catch (error) {
+		// error handling using a middleware
+		next(error);
+	}
+};
--- backend/routes/room-routes.js
+++ backend/routes/room-routes.js
@@ -1,15 +1,29 @@
 import express from 'express';
+import {
+	createRoom,
+	deleteRooms,
+	getAllRooms,
+	getUniqueRoom,
+	updateRoom,
+} from '../controllers/room-controller.js';
+import { verifyCheckAdmin } from '../utils/verifyToken.js';
 
 const roomRouter = express.Router();
 
-// http://localhost:5000/api/auth
-roomRouter.get('/', (req, res, next) => {
-	res.send('Hello this is the room end point');
-});
+// post request to a new hotel and we check if the user is a admin to add new hotels
+roomRouter.post('/:hotelId', verifyCheckAdmin, createRoom);
 
-// http://localhost:5000/api/auth/register
-roomRouter.get('/register', (req, res, next) => {
-	res.send('Hello this is the reg end point');
-});
+// update request to a existing hotel and we check if the user is an admin to update hotels
+roomRouter.put('/:id', verifyCheckAdmin, updateRoom);
+
+// delete request to a existing hotel and we check if the user is an admin to delete hotels
+// room id and then hotel id to delete a room
+roomRouter.delete('/:id/:hotelId', verifyCheckAdmin, deleteRooms);
+
+// get a specific hotel request to existing hotels
+roomRouter.get('/:id', getUniqueRoom);
+
+// getting all hotel from the existing hotels
+roomRouter.get('/', getAllRooms);
 
 export default roomRouter;
